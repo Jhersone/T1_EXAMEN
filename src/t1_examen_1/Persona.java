931bf16167e7aa8f80bb304d2418c3fb21a7e597
Jhersone <142633116+Jhersone@users.noreply.github.com>
2023-09-08 13:45:04 -0500

    Add files via upload

---
+++ src/t1_examen_1/Persona.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1_examen_1;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Persona {
+    
+    private String nombre;
+ 
+    public Persona(String nombre) {
+        this.nombre = nombre;
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+                  
+                    
+}
